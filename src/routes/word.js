bf242b5073b66b41dac943fa7ce0f920deb73a00
Matheus OLiveira <olivermathe16@gmail.com>
2019-08-28 16:43:56 -0300

    adicionanco @hapi/joi

--- src/routes/word.js
+++ src/routes/word.js
@@ -1,3 +1,5 @@
+const Joi = require('@hapi/joi');
+
 const { wordHandler } = require('../handlers');
 
 module.exports = [
@@ -7,6 +9,19 @@ module.exports = [
     handler: wordHandler.createNewWord,
     options: {
       description: 'adiciona uma nova palavra',
+      validate: {
+        payload: Joi.object({
+          name: Joi.string().example('teste').required(),
+          catagoryId: Joi.number().positive().example(12).required()
+        }).label('PUT-word'),
+      },
+      response: {
+        schema: Joi.object({
+          id: Joi.number().positive().example(23),
+          name: Joi.string().example('teste'),
+          catagoryId: Joi.number().positive().example(12)
+        }).label('RES-PUT-word'),
+      }
     }
   }
 ];
